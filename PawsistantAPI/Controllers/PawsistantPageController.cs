bd778706e2648aa369a78c94c3ea16666bed87ea
marlenhalvorsen <marlenhalvorsen@gmail.com>
2025-05-27 20:27:34 +0200

    Added folder for testing

--- PawsistantAPI/Controllers/PawsistantPageController.cs
+++ PawsistantAPI/Controllers/PawsistantPageController.cs
@@ -2,8 +2,11 @@
 
 namespace PawsistantAPI.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class PawsistantPageController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
